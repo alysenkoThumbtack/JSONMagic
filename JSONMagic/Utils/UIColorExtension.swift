bc5a6d2b1fa7bfd5a51fab882da48119398164a2
alysenkoThumbtack <alysenko@thumbtack.net>
2016-02-16 15:14:24 +0600

    Stage2: complex data types, custom transformers, keypath

---
+++ JSONMagic/Utils/UIColorExtension.swift
@@ -0,0 +1,58 @@
+//
+//  UIColorExtension.swift
+//  RSBarcodesSample
+//
+//  Created by R0CKSTAR on 6/13/14.
+//  Copyright (c) 2014 P.D.Q. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    convenience init(hex: String) {
+        var red: CGFloat   = 0.0
+        var green: CGFloat = 0.0
+        var blue: CGFloat  = 0.0
+        var alpha: CGFloat = 1.0
+        
+        if hex.hasPrefix("#") {
+            let hex = hex.substringFromIndex(hex.startIndex.advancedBy(1))
+            let scanner = NSScanner(string: hex)
+            var hexValue: CUnsignedLongLong = 0
+            
+            if scanner.scanHexLongLong(&hexValue) {
+                if hex.length() == 6 {
+                    red   = CGFloat((hexValue & 0xFF0000) >> 16) / 255.0
+                    green = CGFloat((hexValue & 0x00FF00) >> 8)  / 255.0
+                    blue  = CGFloat(hexValue & 0x0000FF) / 255.0
+                } else if hex.length() == 8 {
+                    red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
+                    green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
+                    blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
+                    alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
+                } else {
+                    print("invalid rgb string, length should be 7 or 9", terminator: "")
+                }
+            } else {
+                print("scan hex error")
+            }
+        } else {
+            print("invalid rgb string, missing '#' as prefix", terminator: "")
+        }
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+    
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        
+        return NSString(format:"#%06x", rgb) as String
+    }
+}
